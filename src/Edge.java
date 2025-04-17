1ecda8f36e0dc61ca51d820938b7437600355dd9
Huthaifa <huzaifaomar3@gmail.com>
2025-04-17 14:50:57 +0200

    ()

---
+++ src/Edge.java
@@ -0,0 +1,18 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Edge {
+   int dist;
+   int weight;
+
+   Edge(int var1, int var2) {
+      this.dist = var1;
+      this.weight = var2;
+   }
+
+   public int getDist() {
+      return this.dist;
+   }
+
+   public int getWeight() {
+      return this.weight;
+   }
+}
